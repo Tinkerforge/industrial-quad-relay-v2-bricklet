1d70915c3c69bc2098984bce38212e6e33c14b1f
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:22:51 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,27 +1,27 @@
 use std::{error::Error, io, thread, time::Duration};
-use tinkerforge::{industrial_quad_relay_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_quad_relay_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Quad Relay Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Quad Relay Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_quad_relay_v2_bricklet = IndustrialQuadRelayV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let iqr = IndustrialQuadRelayV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Turn relays alternating on/off 10 times with 100 ms delay
     for i in 0..10 {
         thread::sleep(Duration::from_millis(100));
-        industrial_quad_relay_v2_bricklet.set_value([true, false, false, false]);
+        iqr.set_value([true, false, false, false]);
         thread::sleep(Duration::from_millis(100));
-        industrial_quad_relay_v2_bricklet.set_value([false, true, false, false]);
+        iqr.set_value([false, true, false, false]);
         thread::sleep(Duration::from_millis(100));
-        industrial_quad_relay_v2_bricklet.set_value([false, false, true, false]);
+        iqr.set_value([false, false, true, false]);
         thread::sleep(Duration::from_millis(100));
-        industrial_quad_relay_v2_bricklet.set_value([false, false, false, true]);
+        iqr.set_value([false, false, false, true]);
     }
 
     println!("Press enter to exit.");
