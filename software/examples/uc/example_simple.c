f9c67c15791a498520f5f6d22df34e93530380b7
Erik Fleckstein <erik@tinkerforge.com>
2020-08-26 10:36:33 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -11,7 +11,6 @@ void example_setup(TF_HalContext *hal) {
 	// Create device object
 	check(tf_industrial_quad_relay_v2_create(&iqr, UID, hal), "create device object");
 
-
 	// Turn relays alternating on/off 10 times with 100 ms delay
 	int i;
 	for(i = 0; i < 10; ++i) {
@@ -28,7 +27,6 @@ void example_setup(TF_HalContext *hal) {
 		bool value_3[4] = {false, false, false, true};
 		check(tf_industrial_quad_relay_v2_set_value(&iqr, value_3), "call set_value");
 	}
-
 }
 
 void example_loop(TF_HalContext *hal) {
