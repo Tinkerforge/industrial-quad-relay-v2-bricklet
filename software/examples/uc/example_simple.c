3b79cb62c3b93af81aa9608094989777182c48e5
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:03:38 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -9,19 +9,19 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_IndustrialQuadRelayV2 iqr;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_industrial_quad_relay_v2_create(&iqr, UID, hal), "create device object");
 
 	// Turn relays alternating on/off 10 times with 100 ms delay
 	int i;
-	for(i = 0; i < 10; ++i) {
+	for (i = 0; i < 10; ++i) {
 		tf_hal_sleep_us(hal, 100 * 1000);
 		bool value[4] = {true, false, false, false};
 		check(tf_industrial_quad_relay_v2_set_value(&iqr, value), "call set_value");
@@ -37,7 +37,7 @@ void example_setup(TF_HalContext *hal) {
 	}
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
